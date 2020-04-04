cb1335b942a2d2d7f4de8a3219dcf11f70aa645d
Viktor Dobroselsky <dobroselsky.viktor@gmail.com>
2020-04-04 18:56:58 +0300

    \"initial\"

---
+++ api-test/src/main/java/com/biuwsi/test.api/ApiTest.java
@@ -0,0 +1,13 @@
+package com.biuwsi.test.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTest {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiTest.class, args);
+    }
+
+}
